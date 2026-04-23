37e24ad76a2f184fcda0143126335c2946cc387a
hajar <blidihajar.07@gmail.com>
2026-04-23 21:01:23 +0100

    First commit

---
+++ src/pages/user/Home.jsx
@@ -0,0 +1,11 @@
+import UserLayout from "../../components/layouts/UserLayout";
+
+function Home() {
+  return (
+    <UserLayout>
+      <h1>Home</h1>
+    </UserLayout>
+  );
+}
+
+export default Home;
